39aff7596446991d205b625d025a12d2ce923769
piyush_lasane <160647609+piyushlasane@users.noreply.github.com>
2024-06-16 23:58:28 +0530

    Update MysteryNumberChallenge.cpp

--- MysteryNumberChallenge.cpp
+++ MysteryNumberChallenge.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main()
 {
-    int guess, num, choice;
+    int guess, number, choice;
     while (choice)
     {
         srand(time(0));
-        num = rand() % 100 + 1;
+        number = rand() % 100 + 1;
         cout << "______________________________________________________________________\n";
         cout << "\nGuess the number between 1 to 100\n";
         for (int i = 1; i != 0; i++)
         {
             cin >> guess;
-            if (guess == num)
+            if (guess == number)
             {
                 cout << "You guessed the correct number in " << i << " attempts";
                 break;
             }
-            else if (guess < num)
+            else if (guess < number)
             {
                 cout << "Higher number please...\n";
             }
